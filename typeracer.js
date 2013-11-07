6b07a7c0a57d19a0c0e9f5842d41dd81f2d4253b
Daniel Kimbel <danielkimbel@Daniels-MacBook-Pro.local>
2013-11-06 22:10:53 -0800

    Implemented more backend features, including countdown timer and automatic stopping of timer when typing is complete. Also added a real-time error alert. Finally, prevented users from starting the game until the countdown is complete.

--- typeracer.js
+++ typeracer.js
@@ -3,36 +3,68 @@ setTimeout(function(){$("#text_to_type").text(allWords)}, 1)
 letterCounter = 0
 correctAttempts = 0
 failedAttempts = 0
-allWords = "Hello there how are you"
+allWords = "Hello there, how are you? There are many things to be typed."
 allLetters = allWords.split("")
 
 function getNextLetter() {
   return allLetters[letterCounter] 
 }
 
+var programStarted = false
+
+startCountdown()
+
 var correctLetter = getNextLetter()
 
+// TODO: make it so keypresses are not tracked until countdown has completed
 $(document).keypress(function(event) {
   var enteredLetterCode = event.keyCode
   var enteredLetter = String.fromCharCode(enteredLetterCode)
   // console.log(enteredLetter)
-  if (enteredLetter === correctLetter) {
-    allLetters[letterCounter] = "<span style='color:red;'>" + allLetters[letterCounter] + "</span>"
+  if ((enteredLetter === correctLetter) && programStarted) {
+    allLetters[letterCounter] = "<span style='color:orange;'>" + allLetters[letterCounter] + "</span>"
     newText = allLetters.join("")
     $("#text_to_type").text("")
     $("#text_to_type").append(newText)
     correctAttempts += 1
     letterCounter += 1
+    checkForCompletion()
     correctLetter = getNextLetter()
-    // CLEAR ERROR RESPONSE HERE
-  } else {
+    $("#error_display").text("")
+  } else if (programStarted) {
     failedAttempts += 1
-    // ADD ERROR RESPONSE HERE
+    $("#error_display").text("ERROR")
   }
 })
 
+function checkForCompletion() {
+  if (letterCounter + 1 > allLetters.length) {
+    endTime = new Date()
+    outputWPM(getTotalTime())
+    outputAccuracy()
+  }
+}
+
+function startCountdown() {
+  var secondsRemaining = 4
+  var countdownTimer = setInterval(timer, 1000)
+  function timer() {
+    secondsRemaining -= 1
+    if (secondsRemaining >= 1) {
+      $("#countdown").text(secondsRemaining + "...")
+    } else if (secondsRemaining === 0) {
+      $("#countdown").text("Go!")
+      startTime = new Date()
+      programStarted = true
+    } else if (secondsRemaining <= -1) {
+      clearInterval(countdownTimer)
+      $("#countdown").text(" ")
+    }
+  }
+}
+
 function getTotalTime(start, stop) {
-  return stop - start
+  return endTime - startTime
 }
 
 function outputWPM(totalTime) {
